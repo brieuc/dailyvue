e5a5e9eb85cbf58220b30a88e52609a15e2a6210
Brieuc GORIN <brieuc.gorin@gmail.com>
2024-07-27 00:27:39 +0200

    summary info

--- src/components/SummaryInfo.vue
+++ src/components/SummaryInfo.vue
@@ -1,12 +1,27 @@
 <template>
 <h4 style="color: blueviolet;">Du {{ fromDate }} au {{ toDate }}</h4>
-<div>Activités sportives {{ sportDuration }} min</div>
-<div>Calories dépensées {{ spentKcal }} kcal</div>
-<div>Calories ingérées {{ ingestedKcal }} kcal</div>
+<div class="flex">
+      <div>Activités sportives {{ sportDuration }} min</div>
+      <div>Calories dépensées {{ spentKcal }} kcal</div>
+      <div>Calories ingérées {{ ingestedKcal }} kcal</div>
+      <div>Alcalories {{ drinkingBeer }} kcal</div>
+      <div>Aerobic {{ aerobic }}</div>
+      <div>Anaerobic {{ anaerobic }}</div>
+</div>
 </template>
 
 <script setup>
 
 import { ref, defineEmits, defineProps, onUpdated, onMounted } from 'vue'
-const props = defineProps(["fromDate", "toDate", "ingestedKcal", "spentKcal", "sportDuration"]);
+const props = defineProps(["fromDate", "toDate", "ingestedKcal", "spentKcal", "sportDuration",
+                              "drinkingBeer", "anaerobic", "aerobic"]);
 </script>
+
+<style>
+.flex {
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    gap: 0.1rem;
+}
+</style>
